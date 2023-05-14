f57b23a36f0d27c4227c70011c5dcdf663354333
ZboSS <gallimoreraheim@gmail.com>
2023-05-14 06:20:05 -0500

    updated get

--- api/app.py
+++ api/app.py
@@ -11,19 +11,19 @@ import datetime
 import pydantic
 import motor.motor_asyncio
 import pytz
+import random
 
 app = FastAPI()
 
 
 origins = [
-    "https://simple-smart-hub-client.netlify.app/",
+    "https://simple-smart-hub-client.netlify.app",
     "http://127.0.0.1:8000"
-
 ]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -44,21 +44,21 @@ geolocator = Nominatim(user_agent="MyApp")
 location = geolocator.geocode("Hyderabad")
 
 
+def get_sunset():
+    user_latitude =  location.latitude
+    user_longitude = location.longitude
 
-user_latitude =  location.latitude
-user_longitude = location.longitude
+    sunset_api_endpoint = f'https://api.sunrise-sunset.org/json?lat={user_latitude}&lng={user_longitude}'
 
-sunset_api_endpoint = f'https://api.sunrise-sunset.org/json?lat={user_latitude}&lng={user_longitude}'
+    sunset_api_response = requests.get(sunset_api_endpoint)
+    sunset_api_data = sunset_api_response.json()
 
-sunset_api_response = requests.get(sunset_api_endpoint)
-sunset_api_data = sunset_api_response.json()
+    sunset_time = datetime.datetime.strptime(sunset_api_data['results']['sunset'], '%I:%M:%S %p').time()
+    
+    return datetime.datetime.strptime(str(sunset_time),"%H:%M:%S")
 
 current_date = datetime.date.today()
-sunset_time = datetime.datetime.strptime(sunset_api_data['results']['sunset'], '%I:%M:%S %p').time()
 now_time = datetime.datetime.now(pytz.timezone('Jamaica')).time()
-
-
-datetime1 = datetime.datetime.strptime(str(sunset_time),"%H:%M:%S")
 datetime2 = datetime.datetime.strptime(str(now_time),"%H:%M:%S.%f")
 
 
@@ -96,19 +96,20 @@ def parse_time(time_str):
 async def home():
     return {"message": "ECSE3038 - Project"}
 
-@app.get('/graph?size={n}')
-async def get_sensor_readings(request: Request, n: int):
-    readings = await sensor_readings.find().to_list(n)
-    result = []
-    for reading in readings:
-        result.append({
-            'id': str(reading['_id']),
-            'temperature': reading['user_temp'],
-            'presence': reading['presence'],
-            'timestamp': reading['timestamp'].isoformat(),
-        })
-    return readings
 
+
+@app.get('/graph')
+async def graph(request: Request):
+    size = int(request.query_params.get('size'))
+    readings = await sensor_readings.find().to_list(size)
+    data = []
+    for reading in readings:
+        data.append({
+            "temperature": reading["user_temp"],
+            "presence": random.choice([True, False]),
+            "datetime":reading["user_light"]
+        })
+    return data
 
 @app.put('/settings')
 async def get_sensor_readings(request: Request):
@@ -118,26 +119,18 @@ async def get_sensor_readings(request: Request):
     user_light = state["user_light"]
     light_time_off = state["light_duration"]
 
-    user_light_scr = datetime.datetime.strptime(user_light, "%H:%M:%S")
+    if user_light == "sunset":
+        user_light_scr = get_sunset()
+    else:
+        user_light_scr = datetime.datetime.strptime(user_light, "%H:%M:%S")
+    
     new_user_light = user_light_scr + parse_time(light_time_off)
 
     output = {
         "user_temp": user_temp,
-        "user_light": user_light,
+        "user_light": str(user_light_scr.time()),
         "light_time_off": str(new_user_light.time())
         }
     new_settings = await sensor_readings.insert_one(output)
     created_settings = await sensor_readings.find_one({"_id":new_settings.inserted_id})
     return created_settings
-
-
-@app.get("/api/state")
-async def get_state():
-  state = await sensor_readings.find_one({"tobe": "updated"})
-  
-  state["fan"] = (float(state["temperature"]) >= 28.0) 
-  state["light"] = (datetime1<datetime2)
-
-  if state == None:
-    return {"fan": False, "light": False}
-  return state
